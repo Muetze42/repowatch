8b9be27621eb9f05046b9a08ef65616a10c5f6a2
Norman Huth <norman@huth.it>
2025-09-17 20:27:07 +0200

    docs: Add @mixin annotation to User model for better IDE support and type hinting

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @mixin \Illuminate\Database\Eloquent\Builder<\App\Models\User>
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
