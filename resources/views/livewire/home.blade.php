66db92411d519037d79f8da6b194869a4b971b75
Norman Huth <norman@huth.it>
2025-09-18 21:46:49 +0200

    feat: Add Livewire Home component and corresponding Blade view for initial setup

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,8 @@
+<flux:main container>
+  Icon Test
+
+  <flux:button icon="laravel.nova" href="/" wire:navigate>Export</flux:button>
+  <flux:button icon="livewire.flux-pro" href="/" wire:navigate>Export</flux:button>
+  <flux:link href="#" variant="external">documentation</flux:link>
+</flux:main>
+<<
